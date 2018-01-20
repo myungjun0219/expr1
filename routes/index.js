b0945ef587e030336ded06fa1eb474a2eb22f5e4
myungjun0219 <myungjun0219@gmail.com>
2018-01-20 17:50:14 +0900

    안녕 nodejs -> 안녕 express 수정

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 //http://localhost:3000/
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '안녕 nodejs!' });
+  res.render('index', { title: '안녕 express' });
 });
 
 module.exports = router;
